8c496d1006774f6d7170d3d1d8701f044b3a8ba6
Juditbr92 <juditbardonromero@gmail.com>
2023-11-05 22:15:06 +0100

    Reto2 bucles while

---
+++ buclesWhile.js
@@ -0,0 +1,13 @@
+// Reto 2: realizar una función que devuelva si existe un numero par en el array de núm introducidos por entrada. 
+var arr = [1, 25, 33, 10, 11];
+function hasEven(myNums) {
+    var i = 0;
+    while (i < myNums.length) {
+        if (myNums[i] % 2 == 0) {
+            return "Hay número par";
+        }
+        i++;
+    }
+    return "No hay número par";
+}
+console.log(hasEven(arr));
