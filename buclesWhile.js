6d11870a2dafd8cceb1afa73908225ae4368405d
Juditbr92 <juditbardonromero@gmail.com>
2023-11-05 23:41:03 +0100

    reto3 bucles while

--- buclesWhile.js
+++ buclesWhile.js
@@ -11,3 +11,18 @@ function hasEven(myNums) {
     return "No hay número par";
 }
 console.log(hasEven(arr));
+// Reto 3: Realizar una función que reciba un array de nombres y te devuelva verdadero sí y solo si todos los nombres empiezan por M
+function startWithM(myNames) {
+    var i = 0;
+    while (i < myNames.length) {
+        if (!myNames[i].startsWith("M")) {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+var nombres = ["Maria", "Marta", "Mercedes", "Laura"];
+var nombres2 = ["Maria", "Marta", "Mercedes", "Monica"];
+console.log(startWithM(nombres));
+console.log(startWithM(nombres2));
--- buclesWhile.ts
+++ buclesWhile.ts
@@ -2,7 +2,7 @@
 
 let arr = [1, 25, 33, 10, 11]
 function hasEven(myNums) :string{
-    let i = 0
+    let i = 0;
     while (i < myNums.length){
         if(myNums[i] % 2 == 0){
             return "Hay número par"
@@ -13,3 +13,22 @@ function hasEven(myNums) :string{
 }
 
 console.log(hasEven(arr));
+
+// Reto 3: Realizar una función que reciba un array de nombres y te devuelva verdadero sí y solo si todos los nombres empiezan por M
+
+function startWithM(myNames: string[]): boolean{
+    let i  = 0;
+    while (i < myNames.length) {
+        if(!myNames[i].startsWith("M")){
+            return false
+        }
+        i ++;
+    }
+    return true 
+}
+
+let nombres = ["Maria", "Marta", "Mercedes", "Laura"]
+let nombres2 = ["Maria", "Marta", "Mercedes", "Monica"]
+
+console.log(startWithM(nombres))
+console.log(startWithM(nombres2))
